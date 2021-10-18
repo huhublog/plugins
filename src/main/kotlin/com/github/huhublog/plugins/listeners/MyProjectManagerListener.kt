a929b575a724ed74fcce4b5570f9816edfa44f36
GitHub Action <action@github.com>
2021-10-18 04:07:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/huhublog/plugins/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/huhublog/plugins/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.huhublog.plugins.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.huhublog.plugins.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/huhublog/plugins/services/MyProjectService.kt
+++ src/main/kotlin/com/github/huhublog/plugins/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.huhublog.plugins.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huhublog.plugins.MyBundle
 
 class MyProjectService(project: Project) {
 
